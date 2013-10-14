d7cca9d5c75e3549081c5f637bc58848830935a8
Gronpipmaster <gronpipmaster@gmail.com>
2013-10-15 01:08:52 +0700

    start orm

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-mgodb
+Mgodb
 =====
 
-Orm for mgo, mongo driver for mongo
+Orm for mongodb, wrapper from labix.org/v2/mgo
+Example usage in http://robfig.github.io/revel/ framework see example/init.revel.go and example/model.revel.go, support hooks,
+
+    func (self *SomeModelName) PreInsert(_ mgodb.DbExecutor) error {}
+    func (self *SomeModelName) PostInsert(_ mgodb.DbExecutor) error {}
+    func (self *SomeModelName) PreUpdate(_ mgodb.DbExecutor) error {}
+    func (self *SomeModelName) PostUpdate(_ mgodb.DbExecutor) error {}
+    func (self *SomeModelName) PreDelete(_ mgodb.DbExecutor) error {}
+    func (self *SomeModelName) PostDelete(_ mgodb.DbExecutor) error {}
+
+Example usage default CRUD:
+
+    err = Dbm.Insert(&models.SomeModelName{Username: "Ale", Password: "+55 53 8116 9639"},
+    &models.SomeModelName{Username: "Cla", Password: "+55 53 8402 8510"})
+    if err != nil {
+      fmt.Fatal(err)
+    }
+
+    var results []models.SomeModelName
+    err = Dbm.Find("users", bson.M{"u": "Ale"}).All(&results)// All methods support from http://godoc.org/labix.org/v2/mgo#Collection.Find
+    if err != nil {
+      fmt.Fatal(err)
+    }
+
+    fmt.Println("Result:", results)
+
--- db.go
+++ db.go
@@ -3,38 +3,52 @@ package mgodb
 import (
 	"errors"
 	"fmt"
-	"github.com/robfig/revel"
 	"labix.org/v2/mgo"
-	"net/url"
+	"labix.org/v2/mgo/bson"
 	"reflect"
 	"time"
 )
 
 var zeroVal reflect.Value
 
+//docs interface Dbm
 type DbExecutor interface {
+	//Wrapper from mgo.Find
 	Find(collectionName string, query interface{}) *mgo.Query
-	Insert(collectionName string, docs ...interface{}) error
-	Update(collectionName string, change interface{}, docs ...interface{}) error
-	Delete(collectionName string, docs ...interface{}) error
+	Insert(docs ...interface{}) error
+	Save(doc interface{}) error
+	Delete(doc interface{}) error
+	//Hooks not affected
+	UpdateAll(collectionName string, selector interface{}, change interface{}) (*mgo.ChangeInfo, error)
+	//Hooks not affected
+	DeleteAll(collectionName string, selector interface{}) (*mgo.ChangeInfo, error)
 }
 
-type MongoDb struct {
+type Dbm struct {
 	Database *mgo.Database
 }
 
-func (self *MongoDb) Init() *MongoDb {
-	self.connect()
-	return self
+func (self *Dbm) Init(connectUrl string, dbName string, timeout time.Duration) (*Dbm, error) {
+	var err error
+	var session *mgo.Session
+	session, err = mgo.DialWithTimeout(connectUrl, timeout*time.Second)
+	session.SetMode(mgo.Monotonic, true)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Could not connect to %s: %s.", connectUrl, err.Error()))
+	}
+	self.Database = session.DB(dbName)
+
+	return self, nil
 }
 
-func (self *MongoDb) Find(collectionName string, query interface{}) *mgo.Query {
+func (self *Dbm) Find(collectionName string, query interface{}) *mgo.Query {
 	return self.collection(collectionName).Find(query)
 }
 
-func (self *MongoDb) Insert(collectionName string, docs ...interface{}) error {
+func (self *Dbm) Insert(docs ...interface{}) error {
 	var err error
 	var elem reflect.Value
+	var collectionName string
 	hookarg := hookArg(DbExecutor(self))
 	for _, doc := range docs {
 		elem, err = self.getPointer(doc)
@@ -42,6 +56,10 @@ func (self *MongoDb) Insert(collectionName string, docs ...interface{}) error {
 			return err
 		}
 		eptr := elem.Addr()
+		collectionName, err = getCollectionName(eptr, hookarg)
+		if err != nil {
+			return err
+		}
 		err = runHook("PreInsert", eptr, hookarg)
 		if err != nil {
 			return err
@@ -58,117 +76,91 @@ func (self *MongoDb) Insert(collectionName string, docs ...interface{}) error {
 	return nil
 }
 
-func (self *MongoDb) Update(collectionName string, change interface{}, docs ...interface{}) error {
+func (self *Dbm) Save(doc interface{}) error {
 	var err error
 	var elem reflect.Value
+	var collectionName string
 	hookarg := hookArg(DbExecutor(self))
-	for _, doc := range docs {
-		elem, err = self.getPointer(doc)
-		if err != nil {
-			return err
-		}
-		eptr := elem.Addr()
-		err = runHook("PreUpdate", eptr, hookarg)
-		if err != nil {
-			return err
-		}
-		_, err = self.collection(collectionName).UpdateAll(doc, change)
-		if err != nil {
-			return err
-		}
-		err = runHook("PostUpdate", eptr, hookarg)
-		if err != nil {
-			return err
-		}
+	elem, err = self.getPointer(doc)
+	if err != nil {
+		return err
+	}
+	eptr := elem.Addr()
+	err = runHook("PreUpdate", eptr, hookarg)
+	if err != nil {
+		return err
+	}
+	collectionName, err = getCollectionName(eptr, hookarg)
+	if err != nil {
+		return err
+	}
+	//TODO auto change all fieds
+	change := bson.M{}
+	err = self.collection(collectionName).Update(doc, change)
+	if err != nil {
+		return err
+	}
+	err = runHook("PostUpdate", eptr, hookarg)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-func (self *MongoDb) Delete(collectionName string, docs ...interface{}) error {
+func (self *Dbm) Delete(doc interface{}) error {
 	var err error
 	var elem reflect.Value
+	var collectionName string
 	hookarg := hookArg(DbExecutor(self))
-	for _, doc := range docs {
-		elem, err = self.getPointer(doc)
-		if err != nil {
-			return err
-		}
-		eptr := elem.Addr()
-		err = runHook("PreDelete", eptr, hookarg)
-		if err != nil {
-			return err
-		}
-		_, err = self.collection(collectionName).RemoveAll(doc)
-		if err != nil {
-			return err
-		}
-		err = runHook("PostDelete", eptr, hookarg)
-		if err != nil {
-			return err
-		}
+	elem, err = self.getPointer(doc)
+	if err != nil {
+		return err
+	}
+	eptr := elem.Addr()
+	err = runHook("PreDelete", eptr, hookarg)
+	if err != nil {
+		return err
+	}
+	collectionName, err = getCollectionName(eptr, hookarg)
+	if err != nil {
+		return err
+	}
+	//TODO getId doc
+	err = self.collection(collectionName).Remove(doc)
+	if err != nil {
+		return err
+	}
+	err = runHook("PostDelete", eptr, hookarg)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-func (self *MongoDb) collection(collectionName string) *mgo.Collection {
+func (self *Dbm) UpdateAll(collectionName string, selector interface{}, change interface{}) (*mgo.ChangeInfo, error) {
+	info, err := self.collection(collectionName).UpdateAll(selector, change)
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
+}
+
+func (self *Dbm) DeleteAll(collectionName string, selector interface{}) (*mgo.ChangeInfo, error) {
+	info, err := self.collection(collectionName).RemoveAll(selector)
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
+}
+
+func (self *Dbm) collection(collectionName string) *mgo.Collection {
 	return self.Database.C(collectionName)
 }
 
-func (self *MongoDb) connect() {
-	var err error
-	var found bool
-	var host, port, user, pass, dbName, timeoutStr string
-	var timeout time.Duration
-	var session *mgo.Session
-
-	connURL := &url.URL{Scheme: "mongodb"}
-
-	if host, found = revel.Config.String("mongo.host"); !found {
-		revel.ERROR.Fatal("No mongo.host found.")
-	}
-
-	if port, found = revel.Config.String("mongo.port"); !found {
-		revel.ERROR.Fatal("No mongo.port found.")
-	}
-
-	if user, found = revel.Config.String("mongo.user"); !found {
-		revel.ERROR.Fatal("No mongo.user found.")
-	}
-
-	if pass, found = revel.Config.String("mongo.pass"); !found {
-		revel.ERROR.Fatal("No mongo.pass found.")
-	}
-
-	if dbName, found = revel.Config.String("mongo.db"); !found {
-		revel.ERROR.Fatal("No mongo.db found.")
-	}
-
-	if timeoutStr, found = revel.Config.String("mongo.timeout"); !found {
-		timeoutStr = "5s"
-	}
-
-	timeout, err = time.ParseDuration(timeoutStr)
-	if err != nil {
-		revel.ERROR.Fatal("mongo.timeout found error, ", err.Error())
-	}
-
-	connURL.Host = fmt.Sprintf("%s:%s", host, port)
-	connURL.User = url.UserPassword(user, pass)
-	connURL.Path = "/" + dbName
-
-	session, err = mgo.DialWithTimeout(connURL.String(), timeout*time.Second)
-	session.SetMode(mgo.Monotonic, true)
-
-	if err != nil {
-		revel.ERROR.Fatalf("Could not connect to %s: %s.", host, err.Error())
-	}
-	self.Database = session.DB(dbName)
-}
-
-func (self *MongoDb) getPointer(doc interface{}) (reflect.Value, error) {
+func (self *Dbm) getPointer(doc interface{}) (reflect.Value, error) {
 	docV := reflect.ValueOf(doc)
 	if docV.Kind() != reflect.Ptr {
-		e := fmt.Sprintf("bv.db: passed non-pointer: %v (kind=%v)", doc,
+		e := fmt.Sprintf("mgodb.Dbm: passed non-pointer: %v (kind=%v)", doc,
 			docV.Kind())
 		return reflect.Value{}, errors.New(e)
 	}
@@ -190,4 +182,15 @@ func runHook(name string, eptr reflect.Value, arg []reflect.Value) error {
 		}
 	}
 	return nil
+}
+
+func getCollectionName(eptr reflect.Value, arg []reflect.Value) (string, error) {
+	fn := eptr.MethodByName("Collection")
+	if fn != zeroVal {
+		ret := fn.Call(arg)
+		if len(ret) > 0 && ret[0].String() != "" {
+			return ret[0].String(), nil
+		}
+	}
+	return "", errors.New(fmt.Sprintf("get Collection:%s err.", eptr.Type().String()))
 }
